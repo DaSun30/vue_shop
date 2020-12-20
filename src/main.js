439f694d06bb2364fde6c4dca318d339c6002f17
curry30sd <59682406+curry30sd@users.noreply.github.com>
2020-12-20 09:16:58 +0800

    完成报表功能开发

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
 
+
 import axios from 'axios'
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -27,6 +28,7 @@ axios.interceptors.request.use(config => {
 })
 // 将axios挂载到vue原型对象上供Login组件通过vue原型调用axios
 Vue.prototype.$http = axios
+
 
 Vue.config.productionTip = false
 
